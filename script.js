8d7186729c922b8a2e3cdf1afee91d9c7bc34d40
fpS-AUDIO <saudio.ru@gmail.com>
2024-02-26 16:57:13 +0100

    final version 1.0

--- script.js
+++ script.js
@@ -5,7 +5,6 @@
 //////////////////////
 
 const form = document.querySelector('.form');
-const workouts = document.querySelector(`.workouts`);
 const containerWorkouts = document.querySelector('.workouts');
 const inputType = document.querySelector('.form__input--type');
 const inputDistance = document.querySelector('.form__input--distance');
@@ -23,16 +22,24 @@ class App {
   // set privat properties
   #map;
   #mapEvent;
+  #zoomLevel = 13;
   #workouts = [];
 
   // indeed constructor is called immediately when an instance is created
   constructor() {
+    // getting user's position
     this.#getPosition();
-    // event listener when change the type of workout <select>, use bind() otherwise `this` points to html element in event listeners
-    inputType.addEventListener(`change`, this.#toggleElevationField.bind(this));
 
-    // event listener when submitting the form, use bind() otherwise `this` points to html element in event listeners
+    // get data from local storage
+    this.#getLocalStorage();
+
+    // event handlers (using bind() otherwise `this` points to html element in event listeners)
+    inputType.addEventListener(`change`, this.#toggleElevationField.bind(this));
     form.addEventListener(`submit`, this.#newWorkout.bind(this));
+    containerWorkouts.addEventListener(
+      `click`,
+      this.#moveMapToWorkout.bind(this)
+    );
   }
 
   #getPosition() {
@@ -56,7 +63,7 @@ class App {
     const longitude = position.coords.longitude;
 
     // create the actual map and store it in #map privat variable
-    this.#map = L.map(`map`).setView([latitude, longitude], 13);
+    this.#map = L.map(`map`).setView([latitude, longitude], this.#zoomLevel);
 
     // generate layer and add it to the map
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -67,6 +74,11 @@ class App {
     // add custom leaflet event listener to map objectuse
     // use bind() otherwise `this`, in event listeners, points to this.#map and not to actual instance
     this.#map.on(`click`, this.#showForm.bind(this));
+
+    // console.log(this.#workouts);
+
+    // looping the array of workouts and show each of their markers on the UI
+    this.#workouts.forEach(workout => this.#createMarker(workout));
   }
 
   #showForm(mapE) {
@@ -76,6 +88,12 @@ class App {
   }
 
   #hideForm() {
+    // clean all the fields of the form
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        ``;
     form.style.display = `none`; // first to eliminate animation
     form.classList.add(`hidden`); // then add class `hidden`
     setTimeout(() => (form.style.display = `grid`), 1000); // so after 1second set display grid as before
@@ -146,19 +164,18 @@ class App {
     // add the create workout instance object to the array of #workouts
     this.#workouts.push(workout);
 
-    // then clean all the fields of the form
-    inputDistance.value =
-      inputDuration.value =
-      inputCadence.value =
-      inputElevation.value =
-        ``;
+    console.log(this.#workouts);
 
     this.#createMarker(workout);
     this.#showWorkout(workout);
+    this.#hideForm();
+    this.#setLocalStorage();
   }
 
   #createMarker(workout) {
-    const { lat, lng } = this.#mapEvent.latlng; // get the coords of the click (use destructuring) from this.#mapEvent
+    // const { lat, lng } = this.#mapEvent.latlng; // get the coords of the click (use destructuring) from this.#mapEvent
+    const [lat, lng] = workout.coords; // get the coords of the click (use destructuring) from this.#mapEvent
+
     // create optional options object for the new popup returned from L.popup({options: here})
     const popUpOptions = {
       maxWidth: 300,
@@ -184,8 +201,10 @@ class App {
   }
 
   #showWorkout(workout) {
+    //<li class="workout workout--${workout.type}" data-id="${workout.getId}">
+
     let htmlTempl = `
-      <li class="workout workout--${workout.type}" data-id="${workout.getId}">
+      <li class="workout workout--${workout.type}" data-id="${workout.id}">
         <h2 class="workout__title">${workout.description}</h2>
         <div class="workout__details">
           <span class="workout__icon">${
@@ -233,15 +252,97 @@ class App {
     }
 
     // insert generate html as last child of workouts container element
-    workouts.insertAdjacentHTML(`beforeend`, htmlTempl);
+    containerWorkouts.insertAdjacentHTML(`beforeend`, htmlTempl);
 
     this.#hideForm();
+  }
+
+  #moveMapToWorkout(e) {
+    // guard
+    if (!e.target.closest(`.workout`)) return;
+
+    // select the actual container of workout and take its id
+    const workoutId = e.target.closest(`.workout`).dataset.id;
+
+    // find the correct workout in the array of workout by id matching
+    const matchingWorkout = this.#workouts.find(
+      // workout => workout.getId === workoutId
+      workout => workout.id === workoutId
+    );
+
+    // use the leaflet library intern methods to move the map
+    // map.setView(<LatLng> center, <Number> zoom, <Zoom/pan options> options?)
+    this.#map.setView(matchingWorkout.coords, this.#zoomLevel, {
+      animate: true,
+      duration: 1,
+    });
+
+    // update clicks attribute of the current workout
+    // matchingWorkout._click();
+  }
+
+  #setLocalStorage() {
+    // converting the array of workouts into a string
+    const workoutStr = JSON.stringify(this.#workouts);
+    // console.log(this.#workouts);
+
+    // then store the key:value pair inside the local storage
+    localStorage.setItem(`workouts`, workoutStr);
+  }
+
+  #getLocalStorage() {
+    // passing the key `workouts` to take data (workouts) from local storage
+    const workoutsLocalStorageSTRING = localStorage.getItem(`workouts`);
+
+    // in case there is not workouts in local storage just return
+    if (!workoutsLocalStorageSTRING) return;
+
+    // converting the string back to the objects
+    const workoutsLocalStorage = JSON.parse(workoutsLocalStorageSTRING);
+
+    // Restore the prototype inheritance
+    this.#workouts = workoutsLocalStorage.map(workout => {
+      let obj;
+      if (workout.type === `running`) {
+        obj = Object.create(Running.prototype); // Create a new object with the Running prototype
+      } else if (workout.type === `cycling`) {
+        obj = Object.create(Cycling.prototype); // Create a new object with the Cycling prototype
+      } else {
+        return workout; // If it's neither, return the plain object
+      }
+      return Object.assign(obj, workout); // Merge the plain object into the prototype-based object
+    });
+
+    /* /////////////////////////////////////////////////////
+    // restore the prototype inheritance
+    workoutsLocalStorage.forEach(workout => {
+      if (workout.type === `running`) {
+        Object.create(Running.prototype)
+      }
+    });
+
+    // restoring the data from
+    this.#workouts = workoutsLocalStorage;
+    */ /////////////////////////////////////////////////////
+
+    // looping the array of workouts and show each of them on the UI
+    this.#workouts.forEach(workout => this.#showWorkout(workout));
+
+    // then also create marker but only once the map is loaded,
+    // so put this logic in the end of this.#loadMap()
+  }
+
+  // Public method to use in console
+  reset() {
+    localStorage.removeItem(`workouts`); // remove all workouts with this key `workouts`
+    location.reload(); // reload the page
   }
 }
 // ---| end architecture |-------------------------------------------------------
 
 class Workout {
-  #id = `${Date.now()}`.slice(-10);
+  id = `${Date.now()}`.slice(-10);
+  #clicks = 0;
   date = new Date();
 
   constructor(parameters) {
@@ -259,8 +360,12 @@ class Workout {
     } ${this.date.getDate()}`;
   }
 
-  get getId() {
-    return this.#id;
+  // get getId() {
+  //   return this.#id;
+  // }
+
+  _click() {
+    this.#clicks++;
   }
 }
 
